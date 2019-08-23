90aefc5521f5cc10399e83d35d86809e3c4deeb1
DewminiSandumi <sandumi93@gmail.com>
2019-08-24 02:33:37 +0530

    Add Splash Screen and Link Pages

--- V3/dhara/src/app/app.component.ts
+++ V3/dhara/src/app/app.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild  } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { timer, from } from 'rxjs';
+
+import { HomePage } from '../app/home/home.page';
+import {PastFloodPage} from '../app/past-flood/past-flood.page';
+import {WaterLevelPage} from '../app/water-level/water-level.page';
+import {WeatherPage} from '../app/weather/weather.page'
+
 
 @Component({
   selector: 'app-root',
@@ -10,18 +18,32 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+
+  rootPage: any = HomePage;
+  showSplash = true;
+  pages: Array<{title: string, component: any}>;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Past Floods', component: PastFloodPage },
+      { title:'Weather', component:WeatherPage},
+      {title:'Water Level',component:WaterLevelPage}
+    ];
     this.initializeApp();
   }
-
+  
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      timer(3000).subscribe(() => this.showSplash = false) // <-- hide animation after 3s
     });
   }
+
 }
